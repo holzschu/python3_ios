d56157fe003e09cab00ed92353111949b0f76eca
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2019-04-08 16:30:34 +0200

    Loading modules dynamically

--- compilePython.sh
+++ compilePython.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xcodebuild -project Python3_ios/Python3_ios.xcodeproj -alltargets -sdk iphoneos -arch arm64 -configuration Debug -quiet
+xcodebuild -project Python3_ios/Python3_ios.xcodeproj -alltargets -sdk iphoneos -arch arm64 -configuration Release -quiet
 
