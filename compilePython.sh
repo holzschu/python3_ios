d900394ab7c79b033b4c363452d2ff103067e5b0
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-23 14:49:56 +0200

    Cleanup with Xcode project + automatic Swift binary package creation

--- compilePython.sh
+++ compilePython.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 xcodebuild -project Python3_ios/Python3_ios.xcodeproj -alltargets -sdk iphoneos -arch arm64 -configuration Release -quiet
+xcodebuild -project Python3_ios/Python3_ios.xcodeproj -alltargets -arch x86_64 -sdk iphonesimulator -configuration Release -quiet
+
+# xcframeworks creation 
+
+for framework in python3_ios pythonA pythonB pythonC pythonD pythonE
+do
+   rm -rf $framework.xcframework
+   xcodebuild -create-xcframework -framework Python3_ios/build//Release-iphoneos/$framework.framework -framework Python3_ios/build//Release-iphonesimulator/$framework.framework -output $framework.xcframework
+   echo $framework
+   rm -f $framework.xcframework.zip
+   zip -rq $framework.xcframework.zip $framework.xcframework
+   swift package compute-checksum $framework.xcframework.zip
+done
 
