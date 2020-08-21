94f6defad0f1de776edc3a693beb5949bc188d35
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-08-21 11:54:53 +0200

    Recompiled all packages with Xcode12b5 + command line

--- Package.swift
+++ Package.swift
@@ -13,48 +13,46 @@ let package = Package(
         .binaryTarget(
             name: "python3_ios",
             url: "https://github.com/holzschu/python3_ios/releases/download/v1.0/python3_ios.xcframework.zip",
-            checksum: "4c2d2e95b230adf62ff516dafae92dea542334a79ee9db2483d6f6ef51555140"
+            checksum: "e221caf400a7a09d0f29a41eb0ab6a9a4f643262c5e4f6899791be3e4a029063"
         ),
         .binaryTarget(
             name: "pythonA",
             url: "https://github.com/holzschu/python3_ios/releases/download/v1.0/pythonA.xcframework.zip",
-            checksum: "4c067470ae5d89f12563cf9430b0a21946faa27b43e757dd84b390ad3addb17b"
+            checksum: "a96ea86e0a037bba4c260501a690145ed1b18bc2fe958ca49034d74ae748fd1b"
         ),
         .binaryTarget(
             name: "pythonB",
             url: "https://github.com/holzschu/python3_ios/releases/download/v1.0/pythonB.xcframework.zip",
-            checksum: "807f1cd53257ba72951e53270a59997cdda6db7664c00aeaf788bedb270f9bcc"
+            checksum: "a52de89c653012a52fed418842491f65b7df1e57b83a18bb2c05e3ba62a7f212"
         ),
         .binaryTarget(
             name: "pythonC",
             url: "https://github.com/holzschu/python3_ios/releases/download/v1.0/pythonC.xcframework.zip",
-            checksum: "b7517fb086f1a13a185ad9d2f932866cc17904a6ae4eb76a139de9edef7e1800"
+            checksum: "8423a5991484d7e95095c7355643e351f1ceed158a7333105a56ac7f7d61b58b"
         ),
         .binaryTarget(
             name: "pythonD",
             url: "https://github.com/holzschu/python3_ios/releases/download/v1.0/pythonD.xcframework.zip",
-            checksum: "84255b7312970c8f94d94c268137135f14bf1c91f9e240b1a1ecebb5cd2d8a4f"
+            checksum: "e1fc10a169f8b77672a52275d2faf0b8bb7896e486e9bc0d06d023666bb55db7"
         ),
         .binaryTarget(
             name: "pythonE",
             url: "https://github.com/holzschu/python3_ios/releases/download/v1.0/pythonE.xcframework.zip",
-            checksum: "be8ab1c89998c8f5d88fb30f46c289a7d77dbc6227d16b4d189b98b89cd0648a"
+            checksum: "0f281485789cfeefd545c3672ef933e3f5e7a38cb92c8440dd5366dbe4c8bb4d"
         )
     ]
 )
 /*
-- recompiled with Xcode 12b5, python3_ios copied 5 times.
-- Removed Module
 python3_ios
-4c2d2e95b230adf62ff516dafae92dea542334a79ee9db2483d6f6ef51555140
+e221caf400a7a09d0f29a41eb0ab6a9a4f643262c5e4f6899791be3e4a029063
 pythonA
-4c067470ae5d89f12563cf9430b0a21946faa27b43e757dd84b390ad3addb17b
+a96ea86e0a037bba4c260501a690145ed1b18bc2fe958ca49034d74ae748fd1b
 pythonB
-807f1cd53257ba72951e53270a59997cdda6db7664c00aeaf788bedb270f9bcc
+a52de89c653012a52fed418842491f65b7df1e57b83a18bb2c05e3ba62a7f212
 pythonC
-b7517fb086f1a13a185ad9d2f932866cc17904a6ae4eb76a139de9edef7e1800
+8423a5991484d7e95095c7355643e351f1ceed158a7333105a56ac7f7d61b58b
 pythonD
-84255b7312970c8f94d94c268137135f14bf1c91f9e240b1a1ecebb5cd2d8a4f
+e1fc10a169f8b77672a52275d2faf0b8bb7896e486e9bc0d06d023666bb55db7
 pythonE
-be8ab1c89998c8f5d88fb30f46c289a7d77dbc6227d16b4d189b98b89cd0648a
+0f281485789cfeefd545c3672ef933e3f5e7a38cb92c8440dd5366dbe4c8bb4d
 */
